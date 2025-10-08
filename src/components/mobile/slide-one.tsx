48faf272a5a4fcff3d691fb19e5c05a47d3bb043
hexsolhamiz <hexsolhamiz@gmail.com>
2025-10-08 23:08:01 +0500

    table previous color readded

--- src/components/mobile/slide-one.tsx
+++ src/components/mobile/slide-one.tsx
@@ -1,22 +1,18 @@
-"use client";
-import React, { useState } from "react";
+
+import React from "react";
 import { Hero } from "../static/hero";
-import { Sidebar } from "../static/sidebar";
 import { Footer } from "../static/footer";
 import { MusicMadness } from "../music/music-madness";
-// import { LiveStream } from "../static/live-stream";
 import { CtaSection } from "../static/cta-section";
 import Sponsers from "../sponsers/sponsers";
 
 export const SlideOne = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div className="min-w-full h-fit-content">
       <Hero onMenuClick={() => {}} />
-      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
       <MusicMadness />
-      {/* <LiveStream videoUrl="https://www.youtube.com/embed/36YnV9STBqc?si=z59ATHulfbim8waW" /> */}
       <Sponsers />
       <CtaSection />
       <Footer />
