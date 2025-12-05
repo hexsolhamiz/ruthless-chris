314ed374060a30f09587b78091c4a06573f88b9f
hexsolhamiz <hexsolhamiz@gmail.com>
2025-12-05 22:53:43 +0500

    new-changes-added

---
+++ src/components/banners/jr-banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const JrBanner = () => {
+  return (
+    <div className='w-full h-[600px] bg-black bg-center bg-cover '
+     style={{backgroundImage: "url('/banners/jr.png')"}}>
+
+    </div>
+  )
+}
---
+++ src/components/banners/seb-banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const SebBanner = () => {
+  return (
+    <div className='w-full h-[600px] bg-black bg-center bg-cover '
+     style={{backgroundImage: "url('/banners/seb.png')"}}>
+
+    </div>
+  )
+}
