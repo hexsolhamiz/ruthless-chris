7398f58d3f942769c0d457c0a0f404713c4a9bc8
hexsolhamiz <hexsolhamiz@gmail.com>
2025-12-10 18:27:32 +0500

    new-videos-uploaded

--- src/components/mobile/slide-eight.tsx
+++ src/components/mobile/slide-eight.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SebBanner } from "../banners/seb-banner";
+import { VideosGallery } from "../videos/videos-gallery";
 
 export const SlideEight = () => {
 
   return (
     <div className="min-w-full flex flex-col items-center justify-center">
       <SebBanner />
-  
+     <VideosGallery />
     </div>
   );
 };
